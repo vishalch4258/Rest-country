a0b3075b3ced8c8d16628ad447ec7b182c0c579f
Your Name <you@example.com>
2020-05-03 20:00:26 +0530

    first

--- src/app/restcountries-http.service.ts
+++ src/app/restcountries-http.service.ts
@@ -1,44 +1,33 @@
 import { Injectable } from '@angular/core';
-// importing HTTP client to make the request
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-//importing observable related code.
-import { Observable } from "rxjs";
-import 'rxjs';
-
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestcountriesHttpService {
 
-  public baseUrl = "https://a2dcb3ff.ngrok.io";
+  public baseUrl = 'https://restcountries.eu/rest/v2';
   sendRegion: any;
 
   constructor(public _http: HttpClient) {
-    console.log('RestcountryHttp service constructor is called.');
-  }
-  // exception handler
-  public handleError(err: HttpErrorResponse) {
-    console.log("Handle error http calls");
-    console.log(err.message);
-    return Observable.throw(err.message);
-  }
+    console.log('service constructor is called.');
+   }
 
-  // search box ..
+   // search box ..
 
-  getRegionCountries(region) {
+   getRegionCountries(region) {
     return this._http.get(`${this.baseUrl}/region/${region}`);
   }
 
-  getCountries() {
+   getCountries() {
     return this._http.get(`${this.baseUrl}/all`);
   }
 
-  getCountry(id) {
+   getCountry(id) {
     return this._http.get(`${this.baseUrl}/alpha?codes=${id}`);
   }
 
-  // method to get all countries
+   // method to get all countries
   public getAllCountries(regionName): any {
     const myResponse = this._http.get(this.baseUrl + '/region' + '/' + regionName);
     return myResponse;
@@ -49,19 +38,18 @@ export class RestcountriesHttpService {
     const myResponse = this._http.get(this.baseUrl + '/name' + '/' + countryName + '?fullText=true');
     return myResponse;
   }
-  // method to get all countries with common currency
-  public getAllCountriesByCurrency(currencyCode): any {
+   // method to get all countries with common currency
+   public getAllCountriesByCurrency(currencyCode): any {
     const myResponse = this._http.get(this.baseUrl + '/currency' + '/' + currencyCode);
     return myResponse;
 
   }
 
-  // method to get all countries with common currency
-  public getAllCountriesBySearch(queryParams): any {
-    const by = queryParams.searchRadio;
-    const search = queryParams.search;
-    return this._http.get(this.baseUrl + `/${by}/` + search, { observe: 'response' });
-    // return this._http.get(this.baseUrl + search, { observe: 'response' });
+   // method to get all countries with common currency
+   public getAllCountriesBySearch(queryParams): any {
+     const by = queryParams.searchRadio;
+     const search = queryParams.search;
+     return this._http.get(this.baseUrl + `/${by}/` + search,{ observe: 'response' });
 
   }
 
